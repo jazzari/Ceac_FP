4dac941079e6785fa91e484a736b618f74ae6702
jazzari <passionjazz@gmail.com>
2021-08-04 18:23:34 +0200

    Add password encryption and admin status to users

--- models/Usuario.php
+++ models/Usuario.php
@@ -3,22 +3,28 @@ namespace app\models;
 use app\core\DbModel;
 
 class Usuario extends DbModel{
+    const ADMIN_INACTIVO = 0;
+    const ADMIN_ACTIVO = 1;
+
     public string $user = '';
     public string $nombre = '';
     public string $apellidos = '';
     public string $correo = '';
     public string $pass = '';
+    public int $admin = self::ADMIN_INACTIVO;
 
     public function tableName(): string{
         return  'usuario';
     }
 
     public function registro(){
+        $this->admin = self::ADMIN_INACTIVO;
+        $this->pass = md5($this->pass);
         return $this->guardar();
     }
 
     public function atributos(): array{
-        return ['user', 'nombre', 'apellidos', 'correo', 'pass'];
+        return ['user', 'nombre', 'apellidos', 'correo', 'pass', 'admin'];
     }
 }
 ?>
